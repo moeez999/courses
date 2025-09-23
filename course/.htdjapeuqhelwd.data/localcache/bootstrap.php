9a0b756156ea649eb49c9676f57864540f63c4f4
Muhammad Moeez Raza <109671851+moeez999@users.noreply.github.com>
2025-09-23 23:57:58 +0500

    fix

--- course/.htdjapeuqhelwd.data/localcache/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-// ********** This file is generated DO NOT EDIT **********
-$CFG->siteidentifier = 'VPL766nrtEtNF2qWRsrFoWVeWOspG9eAcourses.latingles.com';
-$CFG->bootstraphash = 'd5191ebf1eb28946d092e9f92392cce5';
-// Only if the file is not stale and has not been defined.
-if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
-    define('SYSCONTEXTID', 1);
-}
